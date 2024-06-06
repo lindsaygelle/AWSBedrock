cbe366234822b0efb0c903e746fad5353ba946f1
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-06 11:28:40 +1000

    Dev (#4)

--- aws_s3_bucket_lifecycle_configuration.tf
+++ aws_s3_bucket_lifecycle_configuration.tf
@@ -1,10 +1,35 @@
 resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail" {
   bucket                = aws_s3_bucket_acl.cloudtrail.bucket
   expected_bucket_owner = aws_s3_bucket_acl.cloudtrail.expected_bucket_owner
+
   rule {
-    expiration {
-      days = 730
+    id     = "STANDARD_IA"
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
     }
+  }
+
+  rule {
+    id     = "INTELLIGENT_TIERING"
+    status = "Enabled"
+    transition {
+      days          = 90
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+
+  rule {
+    id     = "GLACIER_IR"
+    status = "Enabled"
+    transition {
+      days          = 180
+      storage_class = "GLACIER_IR"
+    }
+  }
+
+  rule {
     id     = "GLACIER"
     status = "Enabled"
     transition {
@@ -12,20 +37,110 @@ resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail" {
       storage_class = "GLACIER"
     }
   }
-}
 
-resource "aws_s3_bucket_lifecycle_configuration" "log" {
-  bucket                = aws_s3_bucket_acl.log.bucket
-  expected_bucket_owner = aws_s3_bucket_acl.log.expected_bucket_owner
   rule {
+    id     = "EXPIRE"
+    status = "Enabled"
     expiration {
       days = 730
     }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "inventory" {
+  bucket                = aws_s3_bucket_acl.inventory.bucket
+  expected_bucket_owner = aws_s3_bucket_acl.inventory.expected_bucket_owner
+
+  rule {
+    id     = "STANDARD_IA"
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  rule {
+    id     = "INTELLIGENT_TIERING"
+    status = "Enabled"
+    transition {
+      days          = 90
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+
+  rule {
+    id     = "GLACIER_IR"
+    status = "Enabled"
+    transition {
+      days          = 180
+      storage_class = "GLACIER_IR"
+    }
+  }
+
+  rule {
     id     = "GLACIER"
     status = "Enabled"
     transition {
       days          = 365
       storage_class = "GLACIER"
+    }
+  }
+
+  rule {
+    id     = "EXPIRE"
+    status = "Enabled"
+    expiration {
+      days = 730
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "log" {
+  bucket                = aws_s3_bucket_acl.log.bucket
+  expected_bucket_owner = aws_s3_bucket_acl.log.expected_bucket_owner
+
+  rule {
+    id     = "STANDARD_IA"
+    status = "Enabled"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  rule {
+    id     = "INTELLIGENT_TIERING"
+    status = "Enabled"
+    transition {
+      days          = 90
+      storage_class = "INTELLIGENT_TIERING"
+    }
+  }
+
+  rule {
+    id     = "GLACIER_IR"
+    status = "Enabled"
+    transition {
+      days          = 180
+      storage_class = "GLACIER_IR"
+    }
+  }
+
+  rule {
+    id     = "GLACIER"
+    status = "Enabled"
+    transition {
+      days          = 365
+      storage_class = "GLACIER"
+    }
+  }
+
+  rule {
+    id     = "EXPIRE"
+    status = "Enabled"
+    expiration {
+      days = 730
     }
   }
 }
