93a5b814578b0544f7cba6e1b5b95b2449b964b2
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-19 12:33:45 +1000

    Dev (#10)

--- aws_api_gateway_deployment.tf
+++ aws_api_gateway_deployment.tf
@@ -6,6 +6,8 @@ resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   triggers = {
     redeployment = sha1(jsonencode([
+      aws_api_gateway_request_validator.validate_request_body,
+      aws_api_gateway_model.text_write_request,
       aws_api_gateway_resource.text,
       aws_api_gateway_method.text_post,
       aws_api_gateway_integration.text_post,
--- aws_api_gateway_integration.tf
+++ aws_api_gateway_integration.tf
@@ -2,15 +2,11 @@ resource "aws_api_gateway_integration" "text_post" {
   credentials             = aws_iam_role.api_gateway_rest_api.arn
   http_method             = aws_api_gateway_method.text_post.http_method
   integration_http_method = "POST"
-  passthrough_behavior    = "NEVER"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
   request_templates = {
-    "application/json" = <<EOF
-#set($input = $input.json('$'))
-{
-   "input": "$util.escapeJavaScript($input).replaceAll("\\'", "'")",
-   "stateMachineArn": "${aws_sfn_state_machine.bedrock_text.arn}"
-}
-EOF
+    "application/json" = templatefile("./api_gateway/rest_api/bedrock/request_template/TextWriteRequest.vtl", {
+      aws_sfn_state_machine_arn = aws_sfn_state_machine.bedrock_text.arn
+    })
   }
   resource_id = aws_api_gateway_method.text_post.resource_id
   rest_api_id = aws_api_gateway_method.text_post.rest_api_id
