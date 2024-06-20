d02ae2bfd067e53852df7a80c37d35a89dbec473
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-20 11:56:37 +1000

    Dev (#14)

--- aws_api_gateway_model.tf
+++ aws_api_gateway_model.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_model" "text_write_request" {
   content_type = "application/json"
-  name         = "Entity"
+  name         = "TextWriteRequest"
   rest_api_id  = aws_api_gateway_rest_api.main.id
   schema       = templatefile("./api_gateway/rest_api/bedrock/model/TextWriteRequest.json", {})
 }
