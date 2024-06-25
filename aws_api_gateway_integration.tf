2a30f57b2902104e0acab14972c0b0d84ff541b6
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-25 01:10:54 +0000

    Dev

--- aws_api_gateway_integration.tf
+++ aws_api_gateway_integration.tf
@@ -56,13 +56,8 @@ resource "aws_api_gateway_integration" "bedrock_amazon_text_post" {
       aws_sfn_state_machine_arn = aws_sfn_state_machine.bedrock_amazon_text.arn
     })
   }
-<<<<<<< HEAD
-  resource_id = aws_api_gateway_method.bedrock_text_post.resource_id
-  rest_api_id = aws_api_gateway_method.bedrock_text_post.rest_api_id
-=======
   resource_id = aws_api_gateway_method.bedrock_amazon_text_post.resource_id
   rest_api_id = aws_api_gateway_method.bedrock_amazon_text_post.rest_api_id
->>>>>>> bebbd36 (Dev)
   type        = "AWS"
   uri         = "arn:aws:apigateway:${data.aws_region.main.name}:states:action/StartSyncExecution"
 }
