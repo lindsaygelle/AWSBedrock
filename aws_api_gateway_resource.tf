0a32c493365039e172584919fedd3c7b7dfd5661
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-25 11:30:02 +1000

    Dev (#17)
    * Dev

--- aws_api_gateway_resource.tf
+++ aws_api_gateway_resource.tf
@@ -1,5 +1,41 @@
-resource "aws_api_gateway_resource" "bedrock_text" {
+resource "aws_api_gateway_resource" "bedrock_amazon" {
   parent_id   = aws_api_gateway_rest_api.bedrock.root_resource_id
-  path_part   = "text"
+  path_part   = "amazon"
   rest_api_id = aws_api_gateway_rest_api.bedrock.id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_image" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon.id
+  path_part   = "image"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_image_image_variation" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon_image.id
+  path_part   = "image_variation"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon_image.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_image_inpainting" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon_image.id
+  path_part   = "inpainting"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon_image.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_image_outpainting" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon_image.id
+  path_part   = "outpainting"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon_image.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_image_text_image" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon_image.id
+  path_part   = "text_image"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon_image.rest_api_id
+}
+
+resource "aws_api_gateway_resource" "bedrock_amazon_text" {
+  parent_id   = aws_api_gateway_resource.bedrock_amazon.id
+  path_part   = "text"
+  rest_api_id = aws_api_gateway_resource.bedrock_amazon.rest_api_id
 }
