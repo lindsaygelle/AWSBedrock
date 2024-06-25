b08076777f94e839ac6410a591672d7b3a348701
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-06-25 01:27:38 +0000

    Dev

--- aws_api_gateway_model.tf
+++ aws_api_gateway_model.tf
@@ -40,10 +40,3 @@ resource "aws_api_gateway_model" "bedrock_amazon_text_write_response" {
   rest_api_id  = aws_api_gateway_rest_api.bedrock.id
   schema       = templatefile("./api_gateway/rest_api/bedrock/model/AmazonTextWriteResponse.json", {})
 }
-
-resource "aws_api_gateway_model" "bedrock_text_write_response" {
-  content_type = "application/json"
-  name         = "TextWriteResponse"
-  rest_api_id  = aws_api_gateway_rest_api.bedrock.id
-  schema       = templatefile("./api_gateway/rest_api/bedrock/model/TextWriteResponse.json", {})
-}
