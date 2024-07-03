b28b666485aaa525d65197d23b0f4cffb2f0a401
Linneeeeeeee <lindsay.gelle@gmail.com>
2024-07-03 01:30:54 +0000

    Dev

--- aws_api_gateway_method_response.tf
+++ aws_api_gateway_method_response.tf
@@ -1,3 +1,13 @@
+resource "aws_api_gateway_method_response" "bedrock_amazon_image_text_image_post_200" {
+  http_method = aws_api_gateway_integration_response.bedrock_amazon_image_text_image_post_200.http_method
+  resource_id = aws_api_gateway_integration_response.bedrock_amazon_image_text_image_post_200.resource_id
+  response_models = {
+    "application/json" = aws_api_gateway_model.bedrock_amazon_image_text_image_write_request.name
+  }
+  rest_api_id = aws_api_gateway_integration_response.bedrock_amazon_image_text_image_post_200.rest_api_id
+  status_code = aws_api_gateway_integration_response.bedrock_amazon_image_text_image_post_200.status_code
+}
+
 resource "aws_api_gateway_method_response" "bedrock_amazon_text_post_200" {
   http_method = aws_api_gateway_integration_response.bedrock_amazon_text_post_200.http_method
   resource_id = aws_api_gateway_integration_response.bedrock_amazon_text_post_200.resource_id
